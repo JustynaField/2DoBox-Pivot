2efb608fe79ca2fcbf42464ef65498b9e35b26a7
Justyna Field <justyna.field@gmail.com>
2017-02-20 15:32:26 -0700

    first-round of code refactoring

--- scripts.js
+++ scripts.js
@@ -1,143 +1,113 @@
-function IdeaObj(id,ideaTitle,ideaBody) {
-  this.id = id
-  this.title = ideaTitle
-  this.body = ideaBody
-  this.quality = ' swill'
+getLocalStorage()
+
+function TaskObj(id,taskTitle,taskBody) {
+  this.id = id;
+  this.title = taskTitle;
+  this.body = taskBody;
+  this.quality = 'swill';
 }
 
 function newIdea(parsedOut) {
-  $('.idea-card-container').prepend(
-  `<div class="idea-card" id="${parsedOut.id}">
-    <div class="card-title-box">
+  $('.display-section').prepend(
+  `<div class="card" id="${parsedOut.id}">
+    <div class="title">
       <h1 class="card-title" contenteditable="true">${parsedOut.title}</h1>
-      <button class="delete-btn" type="button" name="button"><img class="quality-image" src="./images/delete.svg" alt="delete button"></img></button>
+      <button class="delete-btn" type="button" name="button"><img class="btn-icon" src="images/delete.svg" alt="delete button"></img></button>
     </div>
+
       <p class="card-body" contenteditable="true">${parsedOut.body}</p>
-    <div class="quality-box">
-      <button class="quality-btns up-vote" type="button" name="button"><img class="quality-image" src="./images/upvote.svg" alt="up vote button"></button>
-      <button class="quality-btns down-vote" type="button" name="button"><img class="quality-image" src="./images/downvote.svg" alt="down vote button"></button>
-      <p class="quality-result">Quality: <p class="current-quality">${parsedOut.quality}</p></p>
-    </div>
+
+    <section class="priority-vote">
+      <button class="vote-btns up-vote" type="button" name="button"><img class="btn-icon" src="images/upvote.svg" alt="up vote button"></button>
+
+      <button class="vote-btns down-vote" type="button" name="button"><img class="btn-icon" src="images/downvote.svg" alt="down vote button"></button>
+      <p class="priority">Quality: <span="priority-level">${parsedOut.quality}</span></p>
+    </section>
   </div>`)
 }
 
 $('.save-button').click(function() {
-  var id = $.now()
-  var ideaTitle = $('.idea-title').val()
-  var ideaBody = $('.idea-body').val()
-  var ideaObj = new IdeaObj(id,ideaTitle,ideaBody)
-  var strungOut = JSON.stringify(ideaObj)
-  localStorage.setItem(id, strungOut)
-  $('.idea-title').val("")
-  $('.idea-body').val("")
-  persistMafk()
+
+  var id = $.now();
+  var taskTitle = $('.task-title').val();
+  var taskBody = $('.task-body').val();
+  var taskObj = new TaskObj(id,taskTitle,taskBody);
+  var stringifyObj = JSON.stringify(taskObj);
+  localStorage.setItem(id, stringifyObj);
+  $('.task-title').val("");
+  $('.task-body').val("");
+  getLocalStorage();
 })
 
-function persistMafk() {
-  $('.idea-card-container').html('')
+function getLocalStorage() {
+  $('.display-section').html('');
   for (var i = 0; i < localStorage.length; i++) {
-    var fromStorage = JSON.parse(
-      localStorage.getItem(
-        localStorage.key(i)
-      ))
-    newIdea(fromStorage)
+    var fromStorage = JSON.parse(localStorage.getItem(localStorage.key(i)));
+    newIdea(fromStorage);
   }
 }
 
-$('.idea-card-container').on('click', '.delete-btn', function() {
-  $(this).parents().remove('.idea-card')
-  var sensitive = $(this).parents('.idea-card').attr('id')
-  localStorage.removeItem(sensitive)
+$('.display-section').on('click', '.delete-btn', function() {
+  $(this).parents().remove('.card');
+  var localStorageKey = $(this).parents('.card').attr('id')
+  localStorage.removeItem(localStorageKey);
 })
 
-$('.idea-card-container').on('click', '.down-vote', function() {
-  var changeQuality = $(this).parents('.idea-card').attr('id')
-  var changeThisQuality = JSON.parse(
-    localStorage.getItem(
-      changeQuality)
-    )
-  var newQual = $(this).siblings('.current-quality')
-  if (newQual.text() == ' genius') {
-    newQual.text(' plausible')
-  } else if (newQual.text() == ' plausible') {
-    newQual.text(' swill')
+$('.display-section').on('click', '.down-vote', function() {
+  var localStorageKey = $(this).parents('.card').attr('id');
+  var localStorageItem = JSON.parse(localStorage.getItem(localStorageKey));
+  var newPriority = $(this).siblings('.priority-level');
+  if (newPriority.text() == ' genius') {
+    newPriority.text(' plausible');
+  } else if (newPriority.text() == ' plausible') {
+    newPriority.text(' swill');
   }
-  changeThisQuality.quality = newQual.text()
-  localStorage.setItem(changeQuality, JSON.stringify(changeThisQuality))
-  persistMafk()
-})
+  localStorageItem.quality = newPriority.text();
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageItem));
+  getLocalStorage();
+});
 
-$('.idea-card-container').on('click', '.up-vote', function() {
-  var changeQuality = $(this).parents('.idea-card').attr('id')
-  var changeThisQuality = JSON.parse(
-    localStorage.getItem(
-      changeQuality)
-    )
-  var newQual = $(this).siblings('.current-quality')
-  if (newQual.text() == ' swill') {
-    newQual.text(' plausible')
-  } else if (newQual.text() == ' plausible') {
-    newQual.text(' genius')
+$('.display-section').on('click', '.up-vote', function() {
+  var localStorageKey = $(this).parents('.card').attr('id');
+  var localStorageItem = JSON.parse(localStorage.getItem(localStorageKey));
+  var newPriority = $(this).siblings('.priority-level');
+  if (newPriority.text() == ' swill') {
+    newPriority.text(' plausible');
+  } else if (newPriority.text() == ' plausible') {
+    newPriority.text(' genius');
   }
-  changeThisQuality.quality = newQual.text()
-  localStorage.setItem(changeQuality, JSON.stringify(changeThisQuality))
-  persistMafk()
-})
+  localStorageItem.quality = newPriority.text();
+  localStorage.setItem(localStorageKey, JSON.stringify(localStorageItem));
+  getLocalStorage();
+});
 
-$('.idea-card-container').on('blur', '.card-title', function() {
-  var updateTitle = $(this).parents('.idea-card').attr('id')
-  var newTitleValue = JSON.parse(
-    localStorage.getItem(
-      updateTitle)
-    )
-  newTitleValue.title = $('.card-title').text()
-  localStorage.setItem(
-    updateTitle,JSON.stringify(
-      newTitleValue)
-    )
-})
 
-$('.idea-card-container').on('blur', '.card-body', function() {
-  var updateBody = $(this).parents('.idea-card').attr('id')
-  var newBodyValue = JSON.parse(
-    localStorage.getItem(
-      updateBody)
-    )
-  newBodyValue.body = $('.card-body').text()
-  localStorage.setItem(
-    updateBody, JSON.stringify(
-      newBodyValue)
-    )
-})
+$('.display-section').on('blur', '.card-title', function() {
+  var localStorageKey = $(this).parents('.card').attr('id');
+  var localStorageItem = JSON.parse(localStorage.getItem(localStorageKey));
+  localStorageItem.title = $('.card-title').text();
+  localStorage.setItem(localStorageKey,JSON.stringify(localStorageItem));
+});
 
-$('.search-text').on('keyup', function(){
-  var lookFor = $(this).val().toLowerCase()
-  $('.idea-card').each(function(index, element){
+$('.display-section').on('blur', '.card-body', function() {
+  var localStorageKey = $(this).parents('.card').attr('id');
+  var localStorageItem = JSON.parse(localStorage.getItem(localStorageKey));
+  localStorageItem.body = $('.card-body').text();
+  localStorage.setItem(localStorageKey,JSON.stringify(localStorageItem));
+});
+
+$('.search-field').on('keyup', function(){
+  var lookFor = $(this).val().toLowerCase();
+  $('.card').each(function(index, element){
     var text = $(element).children().text().toLowerCase();
-    var match = !!text.match(lookFor);
-    $(element).toggle(match);
+    var unmatch = !!text.match(lookFor);
+    $(element).toggle(unmatch);
   })
 })
 
-$('.idea-card-container').on('keypress','.card-title, .card-body', function(e){
+$('.display-section').on('keypress','.card-title, .card-body', function(e){
   if (e.which === 13){
     e.preventDefault()
   $('.card-title, .card-body').blur()
   }
 })
-
-$('.idea-title').on('keypress', function(e) {
-  if (e.which === 13) {
-    e.preventDefault()
-  $('.save-button').click()
-  }
-})
-
-$('.idea-body').on('keypress', function(e) {
-  if (e.which === 13) {
-    e.preventDefault()
-  $('.save-button').click()
-  }
-})
-
-persistMafk()
